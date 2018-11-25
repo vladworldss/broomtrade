25b931c914b887ffc3a93177317082674dcca96c
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-11-25 14:33:06 +0300

    глава 9. добавлен пагинатор.

--- page/views.py
+++ page/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator, InvalidPage
 from .models import Category,Good
 
 
 def index(request, cat_id):
+    try:
+        page_num = request.GET['page']
+    except KeyError:
+        page_num = 1
+
     cats = Category.objects.all().order_by('name')
     if not cat_id:
         cat = Category.objects.first()
     else:
         cat = Category.objects.get(pk=cat_id)
-    goods = Good.objects.filter(category=cat).order_by('name')
+    # make pagination
+    pag = Paginator(Good.objects.filter(category=cat).order_by('name'), 10)
+    try:
+        goods = pag.page(page_num)
+    except InvalidPage:
+        goods = pag.page(1)
 
     return render(request, 'index.html',
                   {'category': cat, 'cats': cats, 'goods': goods}
