b1ff682bb98095aef7a16977b433dc5d4418f490
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-11-25 03:42:47 +0300

    галава 7. начальные шаблоны.

--- page/views.py
+++ page/views.py
@@ -1,17 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Category,Good
 
 
 def index(request, cat_id):
+    cats = Category.objects.all().order_by('name')
     if not cat_id:
         cat = Category.objects.first()
     else:
         cat = Category.objects.get(pk=cat_id)
     goods = Good.objects.filter(category=cat).order_by('name')
-    s = 'Категория: ' + cat.name + '<br><br>'
-    for good in goods:
-        s = s + '(' + str(good.pk) + ')' + good.name + '<br>'
-    return HttpResponse(s)
+
+    return render(request, 'index.html',
+                  {'category': cat, 'cats': cats, 'goods': goods}
+                  )
 
 
 def good(request, good_id):
@@ -19,7 +21,4 @@ def good(request, good_id):
         good = Good.objects.get(pk=good_id)
     except Good.DoesNotExist:
         raise Http404
-    s = good.name + '<br><br>' + good.category.name + '<br><br>' + good.description
-    if not good.in_stock:
-        s = s + '<br><br>' + 'Нет в наличии'
-    return HttpResponse(s)
+    return render(request, 'good.html',{'good': good})
