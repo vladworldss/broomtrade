5a263e8ab3edcf7c8348b599b51b1b2e8489cc67
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-11-25 14:47:43 +0300

    глава 9. воврат на корректную странису списка

--- page/views.py
+++ page/views.py
@@ -9,14 +9,13 @@ def index(request, cat_id):
         page_num = request.GET['page']
     except KeyError:
         page_num = 1
-
     cats = Category.objects.all().order_by('name')
     if not cat_id:
         cat = Category.objects.first()
     else:
         cat = Category.objects.get(pk=cat_id)
     # make pagination
-    pag = Paginator(Good.objects.filter(category=cat).order_by('name'), 10)
+    pag = Paginator(Good.objects.filter(category=cat).order_by('name'), 2)
     try:
         goods = pag.page(page_num)
     except InvalidPage:
@@ -28,9 +27,13 @@ def index(request, cat_id):
 
 
 def good(request, good_id):
+    try:
+        page_num = request.GET['page']
+    except KeyError:
+        page_num = 1
     cats = Category.objects.all().order_by('name')
     try:
         good = Good.objects.get(pk=good_id)
     except Good.DoesNotExist:
         raise Http404
-    return render(request, 'good.html', {'cats': cats, 'good': good})
+    return render(request, 'good.html', {'cats': cats, 'good': good, 'pn': page_num})
