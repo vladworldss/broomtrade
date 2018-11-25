550f5f77773fe2bbac28f9ce9fd7c8abcb98f1f7
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-11-25 13:54:36 +0300

    глава 8. формирование адресов через переменные шаблона

--- broom_trade/settings.py
+++ broom_trade/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
