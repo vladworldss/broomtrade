40c587e11a4922265a4316cf4b9e7d7f8c88222d
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-11-25 15:38:28 +0300

    глава 10. вывод на основе класса-контроллера ListView.

--- page/twviews.py
+++ page/twviews.py
@@ -1,28 +1,32 @@
 from django.views.generic.base import TemplateView
 from django.http import Http404
 from django.core.paginator import Paginator, InvalidPage
+from django.views.generic.list import ListView
 from .models import Category, Good
 
 
-class GoodListView(TemplateView):
+class GoodListView(ListView):
     template_name = 'index.html'
+    queryset = Good.objects.order_by('name')
+    paginate_by = 1
+    cat = None
+
+    def get(self, request, *args, **kwargs):
+        if self.kwargs['cat_id'] is None:
+            self.cat = Category.objects.first()
+        else:
+            self.cat = Category.objects.get(pk=self.kwargs['cat_id'])
+        return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        page_num = self.request.GET.get('page', 1)
         context['cats'] = Category.objects.order_by('name')
-        if kwargs['cat_id'] is None:
-            context['category'] = Category.objects.first()
-        else:
-            context['category'] = Category.objects.get(pk=kwargs['cat_id'])
-
-        _goods = Good.objects.filter(category=context['category']).order_by('name')
-        pag = Paginator(_goods, 1)
-        try:
-            context['goods'] = pag.page(page_num)
-        except InvalidPage:
-            context['goods'] = pag.page(1)
+        context['category'] = self.cat
         return context
+
+    def get_queryset(self):
+        # вызывается позже, чем get_context_data --> self.cat уже будет
+        return Good.objects.filter(category=self.cat).order_by('name')
 
 
 class GoodDetailView(TemplateView):
