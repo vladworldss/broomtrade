991570aa2a567071ac787dae73e0fa1cbe1c516c
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-12-04 23:11:10 +0300

    глава 14. аутентификация и авторизация

--- page/urls.py
+++ page/urls.py
@@ -1,4 +1,5 @@
 from django.urls import re_path
+from django.contrib.auth.decorators import login_required, permission_required
 from page import views
 from page.twviews import (GoodListView, GoodDetailView, GoodCreate,
                           GoodUpdate, GoodDelete, _GoodCreate, CategoryListView, _GoodUpdate
@@ -6,13 +7,21 @@ from page.twviews import (GoodListView, GoodDetailView, GoodCreate,
 
 urlpatterns = [
     # re_path(r'^(?:(?P<cat_id>\d+)/)?$', views.index, name='index'),
-    re_path(r'^(?:(?P<cat_id>\d+)/)?$', GoodListView.as_view(), name='index'),
+    re_path(r'^(?:(?P<cat_id>\d+)/)?$', login_required(GoodListView.as_view()), name='index'),
 
     # re_path(r'^good/(?P<good_id>\d+)/$', views.good, name="good")
     re_path(r'^good/(?P<good_id>\d+)/$', GoodDetailView.as_view(), name="good"),
-    re_path(r'^(?P<cat_id>\d+)/add/$', _GoodCreate.as_view(), name='good_add'),
-    re_path(r'^good/(?P<good_id>\d+)/edit/$', _GoodUpdate.as_view(), name='good_edit'),
-    re_path(r'^good/(?P<good_id>\d+)/delete/$', GoodDelete.as_view(), name='good_delete'),
+    re_path(r'^(?P<cat_id>\d+)/add/$',
+            permission_required("page.add_good")(_GoodCreate.as_view()),
+            name='good_add'),
+
+    re_path(r'^good/(?P<good_id>\d+)/edit/$',
+            permission_required("page.change_good")(_GoodUpdate.as_view()),
+            name='good_edit'),
+
+    re_path(r'^good/(?P<good_id>\d+)/delete/$',
+            permission_required("page.delete_good")(GoodDelete.as_view()),
+            name='good_delete'),
 
     re_path(r'^cats/(?:(?P<cat_id>\d+)/)?$', CategoryListView.as_view(), name='categories'),
 ]
