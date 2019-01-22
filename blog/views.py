d34837b2290f510bb2e284e068783cea823efad0
Vladimir Gerasimenko <vladworldss@yandex.ru>
2019-01-23 00:17:58 +0300

    глава 26. контроллер списка статей.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
+from django.views.generic.dates import ArchiveIndexView
 from django.views.generic.base import ContextMixin
+from django.db.models import Q
+
+from blog.models import Blog
+from generic.controllers import PageNumberView
+from generic.mixins import CategoryListMixin
 
 
 class SearchMixin(ContextMixin):
@@ -10,3 +16,22 @@ class SearchMixin(ContextMixin):
         context["search"] = self.search
         context["tag"] = self.tag
         return context
+
+
+class BlogListView(PageNumberView, ArchiveIndexView, SearchMixin, CategoryListMixin):
+    model = Blog
+    date_field = "posted"
+    template_name = "blog_index.html"
+    paginate_by = 2
+    allow_empty = True
+    allow_future = True
+
+    def get_queryset(self):
+        blog = super().get_queryset()
+        if self.search:
+            blog = blog.filter(Q(title__contains=self.search) |
+                               Q(description__contains=self.search) |
+                               Q(content__contains=self.search))
+        if self.tag:
+            blog = blog.filter(tags__name=self.tag)
+        return blog
